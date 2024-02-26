9175fc8dcac9b51439bbd98ba955f2f94d6ec7e1
Hugo Uittenbosch <hugo.uittenbosch@dlr.de>
2024-02-26 19:35:21 +0100

    9 beam basis

--- src/Ray Tracing/System.jl
+++ src/Ray Tracing/System.jl
@@ -317,10 +317,14 @@ end
 function trace_system!(system::AbstractSystem, agb::AstigmaticGaussianBeamlet; r_max::Int = 20)
     # placeholder solver
     trace_system!(system, agb.c, r_max=r_max)
-    trace_system!(system, agb.wx, r_max=r_max)
-    trace_system!(system, agb.wy, r_max=r_max)
-    trace_system!(system, agb.dx, r_max=r_max)
-    trace_system!(system, agb.dy, r_max=r_max)
+    trace_system!(system, agb.wxp, r_max=r_max)
+    trace_system!(system, agb.wxm, r_max=r_max)
+    trace_system!(system, agb.wyp, r_max=r_max)
+    trace_system!(system, agb.wym, r_max=r_max)
+    trace_system!(system, agb.dxp, r_max=r_max)
+    trace_system!(system, agb.dxm, r_max=r_max)
+    trace_system!(system, agb.dyp, r_max=r_max)
+    trace_system!(system, agb.dym, r_max=r_max)
 end
 
 """
