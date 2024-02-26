b55fa0423fa0ef1fccf806c28e4e2fdae9158a8a
Hugo Uittenbosch <hugo.uittenbosch@dlr.de>
2024-02-26 19:35:21 +0100

    9 beam basis

--- src/Ray Tracing/AstigmaticGaussian.jl
+++ src/Ray Tracing/AstigmaticGaussian.jl
@@ -1,22 +1,30 @@
 mutable struct AstigmaticGaussianBeamlet{T} <: AbstractBeam{T, PolarizedRay{T}}
     id::UUID
     c::Beam{T, PolarizedRay{T}}       # chief
-    wx::Beam{T, Ray{T}}               # waist x
-    wy::Beam{T, Ray{T}}               # waist y
-    dx::Beam{T, Ray{T}}               # div. x
-    dy::Beam{T, Ray{T}}               # div. y
+    wxp::Beam{T, Ray{T}}              # waist x +
+    wxm::Beam{T, Ray{T}}              # waist x -
+    wyp::Beam{T, Ray{T}}              # waist y +
+    wym::Beam{T, Ray{T}}              # waist y -
+    dxp::Beam{T, Ray{T}}              # div. x +
+    dxm::Beam{T, Ray{T}}              # div. x -
+    dyp::Beam{T, Ray{T}}              # div. y +
+    dym::Beam{T, Ray{T}}              # div. y -
     parent::Nullable{AstigmaticGaussianBeamlet{T}}
     children::Vector{AstigmaticGaussianBeamlet{T}}
 end
 
 function AstigmaticGaussianBeamlet(
     c::Beam{T, PolarizedRay{T}},
-    wx::Beam{T, Ray{T}},
-    wy::Beam{T, Ray{T}},
-    dx::Beam{T, Ray{T}},
-    dy::Beam{T, Ray{T}}
+    wxp::Beam{T, Ray{T}},
+    wxm::Beam{T, Ray{T}},
+    wyp::Beam{T, Ray{T}},
+    wym::Beam{T, Ray{T}},
+    dxp::Beam{T, Ray{T}},
+    dxm::Beam{T, Ray{T}},
+    dyp::Beam{T, Ray{T}},
+    dym::Beam{T, Ray{T}}
     ) where {T <: Real}
-return AstigmaticGaussianBeamlet{T}(uuid4(), c, wx, wy, dx, dy,
+return AstigmaticGaussianBeamlet{T}(uuid4(), c, wxp, wxm, wyp, wym, dxp, dxm, dyp, dym,
     nothing,
     Vector{AstigmaticGaussianBeamlet{T}}())
 end
@@ -27,23 +35,44 @@ function AstigmaticGaussianBeamlet(
     λ = 1000e-9,
     w0 = 1e-3;
     M2 = 1,
-    E0 = [0, 0, 1])
+    E0 = [0, 0, 1],
+    support = [0,1,0])
     # Create orthogonal vectors for construction purposes (right-handed)
-    s1 = [0,1,0] # normal3d(direction)
+    if isnothing(support)
+        s1 = normal3d(direction)
+    else
+        s1 = normalize(support)
+    end
     s2 = cross(direction, s1)
     # Divergence angle in rad
     θ = divergence_angle(λ, w0, M2)
     # Waist rays
-    wx = Ray(position + s1 * w0, direction, λ)
-    wy = Ray(position + s2 * w0, direction, λ)
+    wxp = Ray(position + s1 * w0, direction, λ)
+    wxm = Ray(position - s1 * w0, direction, λ)
+    wyp = Ray(position + s2 * w0, direction, λ)
+    wym = Ray(position - s2 * w0, direction, λ)
     # Divergence ray
-    div_dir_x = normalize(direction + s1 * tan(θ))
-    div_dir_y = normalize(direction + s2 * tan(θ))
-    dx = Ray(position, div_dir_x, λ)
-    dy = Ray(position, div_dir_y, λ)
+    div_dir_xp = normalize(direction + s1 * tan(θ))
+    div_dir_xm = normalize(direction - s1 * tan(θ))
+    div_dir_yp = normalize(direction + s2 * tan(θ))
+    div_dir_ym = normalize(direction - s2 * tan(θ))
+    dxp = Ray(position, div_dir_xp, λ)
+    dxm = Ray(position, div_dir_xm, λ)
+    dyp = Ray(position, div_dir_yp, λ)
+    dym = Ray(position, div_dir_ym, λ)
     # Chief ray
     c = PolarizedRay(position, direction, λ, E0)
-    return AstigmaticGaussianBeamlet(Beam(c), Beam(wx), Beam(wy), Beam(dx), Beam(dy))
+    return AstigmaticGaussianBeamlet(
+        Beam(c),
+        Beam(wxp),
+        Beam(wxm),
+        Beam(wyp),
+        Beam(wym),
+        Beam(dxp),
+        Beam(dxm),
+        Beam(dyp),
+        Beam(dym)
+    )
 end
 
 _last_beam_intersection(agb::AstigmaticGaussianBeamlet) = intersection(last(rays(agb.c)))
@@ -56,19 +85,21 @@ function gauss_parameters(agb::AstigmaticGaussianBeamlet, z::Real)
     n = refractive_index(agb.c.rays[i])
     λ = wavelength(agb.c.rays[i])
 
-    wx, ~, ~, ~, Hnx = gauss_parameters(agb.c.rays[i], agb.wx.rays[i], agb.dx.rays[i], p0)
-    wy, ~, ~, ~, Hny = gauss_parameters(agb.c.rays[i], agb.wy.rays[i], agb.dy.rays[i], p0)
+    wxp, ~, ~, ~, Hnxp = gauss_parameters(agb.c.rays[i], agb.wxp.rays[i], agb.dxp.rays[i], p0)
+    wxm, ~, ~, ~, Hnxm = gauss_parameters(agb.c.rays[i], agb.wxm.rays[i], agb.dxm.rays[i], p0)
+    wyp, ~, ~, ~, Hnyp = gauss_parameters(agb.c.rays[i], agb.wyp.rays[i], agb.dyp.rays[i], p0)
+    wym, ~, ~, ~, Hnym = gauss_parameters(agb.c.rays[i], agb.wym.rays[i], agb.dym.rays[i], p0)
 
-    return wx, wy, Hnx*n, Hny*n
+    return wxp, wxm, wyp, wym
 end
 
 function gauss_parameters(agb::AstigmaticGaussianBeamlet{G}, zs::AbstractArray) where {G}
-    wx = Vector{G}(undef, length(zs))
-    wy = Vector{G}(undef, length(zs))
-    Hnx = Vector{G}(undef, length(zs))
-    Hny = Vector{G}(undef, length(zs))
+    wxp = Vector{G}(undef, length(zs))
+    wxm = Vector{G}(undef, length(zs))
+    wyp = Vector{G}(undef, length(zs))
+    wym = Vector{G}(undef, length(zs))
     for (i, z) in enumerate(zs)
-        wx[i], wy[i], Hnx[i], Hny[i] = gauss_parameters(agb, z)
+        wxp[i], wxm[i], wyp[i], wym[i] = gauss_parameters(agb, z)
     end
-    return wx, wy, Hnx, Hny
+    return wxp, wxm, wyp, wym
 end
