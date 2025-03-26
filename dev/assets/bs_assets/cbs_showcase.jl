3fd7587c6e0936f744370850bf017045f6f77f9e
Documenter.jl <documenter@juliadocs.github.io>
2025-03-26 13:07:55 +0000

    build based on 986cf78

---
+++ dev/assets/bs_assets/cbs_showcase.jl
@@ -0,0 +1,51 @@
+using CairoMakie, BeamletOptics
+
+file_dir = @__DIR__
+
+mm = 1e-3
+n = 1.5
+
+cbs1 = CubeBeamsplitter(BeamletOptics.inch, _->n)
+cbs1_mount = MeshDummy(joinpath(file_dir, "CBS Mount.stl"))
+
+cbs2 = CubeBeamsplitter(BeamletOptics.inch, _->n)
+cbs2_mount = MeshDummy(joinpath(file_dir, "CBS Mount.stl"))
+zrotate3d!(cbs2_mount, π)
+
+cbs1_assembly = ObjectGroup([cbs1, cbs1_mount])
+cbs2_assembly = ObjectGroup([cbs2, cbs2_mount])
+
+m1 = RightAnglePrismMirror(BeamletOptics.inch, BeamletOptics.inch)
+m1_mount = MeshDummy(joinpath(file_dir, "CBS Mount.stl"))
+zrotate3d!(m1, π)
+
+m2 = RightAnglePrismMirror(BeamletOptics.inch, BeamletOptics.inch)
+m2_mount = MeshDummy(joinpath(file_dir, "CBS Mount.stl"))
+zrotate3d!(m2, π)
+
+m1_assembly = ObjectGroup([m1, m1_mount])
+m2_assembly = ObjectGroup([m2, m2_mount])
+
+translate_to3d!(cbs2_assembly, [-100mm, 150mm, 0])
+
+translate_to3d!(m1_assembly, [-100mm, 0, 0])
+zrotate3d!(m1_assembly, deg2rad(-45))
+
+translate_to3d!(m2_assembly, [0, 150mm, 0])
+zrotate3d!(m2_assembly, deg2rad(135))
+
+system = System([cbs1_assembly, cbs2_assembly, m1_assembly, m2_assembly])
+
+beam = GaussianBeamlet([0,-60mm,0], [0, 1, 0], 1e-6, 1e-3)
+
+solve_system!(system, beam)
+
+fig = Figure(size=(600, 470))
+ax = Axis3(fig[1,1]; aspect=:data,  azimuth=0, elevation=pi/2)
+
+render_object!.(ax, [cbs1_mount, cbs2_mount, m1_mount, m2_mount])
+render_object!.(ax, [cbs1, cbs2, m1, m2])
+render_beam!(ax, beam, flen=40mm, color=:red)
+
+hidedecorations!(ax)
+hidespines!(ax)
