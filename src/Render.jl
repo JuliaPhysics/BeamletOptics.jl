8211902f4f12b86c6dcc233b51d96c067c28eca3
StackEnjoyer <93778924+StackEnjoyer@users.noreply.github.com>
2025-04-14 11:49:52 +0200

    mesh normal vector plot

--- src/Render.jl
+++ src/Render.jl
@@ -12,20 +12,3 @@ mutable struct MissingBackendError <: RenderException
 end
 
 render!(::Any, ::Any, kwargs...) = throw(MissingBackendError())
-
-# """
-#     render_object_normals!(axis, mesh::Mesh; l=0.01)
-
-# Helper function to visualize the `mesh` normals of an object. The normals are displayed with a standard `l`ength of 0.01
-# """
-# function render_object_normals!(axis, mesh::Mesh; l = 0.01)
-#     for fID in 1:size(mesh.faces)[1]
-#         nml = normal3d(mesh, fID)
-#         pos = @view mesh.vertices[mesh.faces[fID, 1], :]
-#         vec = pos + l * nml
-#         _render_object_normal!(axis, pos, vec; color=:blue)
-#     end
-#     return nothing
-# end
-# _render_object_normal!(::Any, ::AbstractVector, ::AbstractVector; color = :blue) = nothing
-
