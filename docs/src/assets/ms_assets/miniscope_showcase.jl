9c395ba1652e6491b61c53f4070658717c0729c1
StackEnjoyer <93778924+StackEnjoyer@users.noreply.github.com>
2025-06-05 18:07:10 +0200

    more docs

--- docs/src/assets/ms_assets/miniscope_showcase.jl
+++ docs/src/assets/ms_assets/miniscope_showcase.jl
@@ -9,7 +9,17 @@ include(joinpath(@__DIR__, "..", "render_utils.jl"))
 const cm = 1e-2
 const mm = 1e-3
 
-const NBK7 = DiscreteRefractiveIndex([532e-9, 1064e-9], [1.5195, 1.5066])
+lens_color() = RGBf(0.678, 0.847, 0.902)
+lens_color(alpha) = RGBAf(0.678, 0.847, 0.902, alpha)
+
+λ_green = 546.1e-9
+λ_red   = 656.3e-9
+lambdas = [λ_green, λ_red]
+NBK7    = DiscreteRefractiveIndex(lambdas, [1.51872, 1.51432]) # https://www.schott.com/shop/advanced-optics/en/Optical-Glass/SCHOTT-N-BK7/c/glass-SCHOTT%20N-BK7%C2%AE
+NSK5    = DiscreteRefractiveIndex(lambdas, [1.59142, 1.58619]) # https://www.schott.com/shop/advanced-optics/en/Optical-Glass/N-SK5/c/glass-N-SK5
+NSF4    = DiscreteRefractiveIndex(lambdas, [1.76164, 1.74719]) # https://www.schott.com/shop/advanced-optics/en/Optical-Glass/N-SF4/c/glass-N-SF4
+NLAK22  = DiscreteRefractiveIndex(lambdas, [1.65391, 1.64760]) # https://www.schott.com/shop/advanced-optics/en/Optical-Glass/N-LAK22/c/glass-N-LAK22
+NLASF44 = DiscreteRefractiveIndex(lambdas, [1.80832, 1.79901]) # https://www.schott.com/shop/advanced-optics/en/Optical-Glass/N-LASF44/c/glass-N-LASF44
 
 ##
 function miniscope_body()
@@ -32,7 +42,7 @@ function objective_lens_1()
     ## Objective lens 1
     surf_1 = CircularFlatSurface(2*1.144mm)
     surf_2 = SphericalSurface(-1.448mm, 2*1.144mm)
-    obj_lens_1 = Lens(surf_1, surf_2, 1.3mm, NBK7)
+    obj_lens_1 = Lens(surf_1, surf_2, 1.3mm, NSK5)
     return obj_lens_1
 end
 
@@ -41,8 +51,8 @@ function objective_lens_2()
     surf_1 = SphericalSurface(38.184mm, 2*1.840mm, 2*2.380mm)
     surf_2 = SphericalSurface(3.467mm, 2*2.060mm, 2*2.380mm)
     surf_3 = SphericalSurface(-5.020mm, 2*2.380mm)
-    dl11 = Lens(surf_1, surf_2, 0.5mm, NBK7)
-    dl12 = Lens(surf_2, surf_3, 2.5mm, NBK7)
+    dl11 = Lens(surf_1, surf_2, 0.5mm, NSF4)
+    dl12 = Lens(surf_2, surf_3, 2.5mm, NLAK22)
     translate3d!(dl12, [0, BMO.thickness(dl11), 0])
     obj_lens_2 = DoubletLens(dl11, dl12)
     return obj_lens_2
@@ -53,8 +63,8 @@ function tube_doublet()
     surf_1 = SphericalSurface(7.744mm, 2*2.812mm, 2*3mm)
     surf_2 = SphericalSurface(-3.642mm, 2*3mm)
     surf_3 = SphericalSurface(-14.413mm, 2*2.812mm, 2*3mm)
-    dl21 = Lens(surf_1, surf_2, 3.4mm, NBK7)
-    dl22 = Lens(surf_2, surf_3, 1.0mm, NBK7)
+    dl21 = Lens(surf_1, surf_2, 3.4mm, NLAK22)
+    dl22 = Lens(surf_2, surf_3, 1.0mm, NSF4)
     translate3d!(dl22, [0, BMO.thickness(dl21), 0])
     tube_lens = DoubletLens(dl21, dl22)
     return tube_lens
@@ -66,9 +76,9 @@ function objective_group()
     tube_lens = tube_doublet()
     translate_to3d!(obj_lens_2, [0, BMO.position(obj_lens_1)[2] + BMO.thickness(obj_lens_1) + 3.344mm, 0])
     translate_to3d!(tube_lens, [0, BMO.position(obj_lens_2)[2] + BMO.thickness(obj_lens_2) + 2mm, 0])
-    objective_group = ObjectGroup([obj_lens_1, obj_lens_2, tube_lens])
-    xrotate3d!(objective_group, deg2rad(90))
-    return objective_group
+    _objective_group = ObjectGroup([obj_lens_1, obj_lens_2, tube_lens])
+    xrotate3d!(_objective_group, deg2rad(90))
+    return _objective_group
 end
 
 function dichroic_filter()
@@ -82,9 +92,39 @@ function dichroic_filter()
     return filter
 end
 
+function emission_filter()
+    ef = Lens(BMO.PlanoSurfaceSDF(1mm, 4mm), NBK7)
+    return ef
+end
+
+function collection_group()
+    ## Collection group
+    ef_1 = emission_filter()
+    ef_2 = emission_filter()
+
+    collect_lens = Lens(
+        SphericalSurface(6.580mm, 4.5mm),
+        SphericalSurface(-6.580mm, 4.5mm),
+        2.6mm,
+        NLASF44
+    )
+
+    translate3d!(collect_lens, [0, BMO.position(ef_1)[2] + BMO.thickness(ef_1) + 0.1mm, 0])
+    translate3d!(ef_2, [0, BMO.position(collect_lens)[2] + BMO.thickness(collect_lens) + 0.25mm, 0])
+
+    collect_group = ObjectGroup([ef_1, collect_lens, ef_2])
+
+    xrotate3d!(collect_group, deg2rad(90))
+    translate3d!(collect_group, [0, 0.332mm, 21.937mm])
+
+    return collect_group
+end
+
 ##
 miniscope = miniscope_body()
-filter = dichroic_filter()
+obj_group = objective_group()
+df = dichroic_filter()
+cg = collection_group()
 
 c_view = [
  -0.481504   0.876444  -4.16334e-17   0.00569819
@@ -95,48 +135,102 @@ c_view = [
 
 fig = Figure(size=(600, 600))
 display(fig)
-brightness = .8
+brightness = 0.8
 ax = LScene(
     fig[1,1],
     scenekw = (
         lights = [
-            DirectionalLight(RGBAf(brightness, brightness, brightness), Vec3f(1, 0, 1))
-            DirectionalLight(RGBAf(brightness, brightness, brightness), Vec3f(0, 1, 1))
+            DirectionalLight(RGBAf(brightness, brightness, brightness), Vec3f(-1, 0, 1))
+            DirectionalLight(RGBAf(brightness, brightness, brightness), Vec3f(0, -1, -1))
         ], # hella weird syntax but whatever
     )
 )
 hide_axis(ax)
-render!(ax, miniscope)
-render!(ax, filter; color=RGBAf(0,0,1,0.25))
+render!(ax, miniscope, transparency=true, alpha=0.05)
+render!(ax, obj_group, transparency=false, color=lens_color())
+render!(ax, df, transparency=false, color=lens_color())
+render!(ax, cg, transparency=false, color=lens_color())
 
 set_view(ax, c_view)
-save("test.png", fig; px_per_unit=8, update = false)
+save("ucla_intro_fig.png", fig; px_per_unit=8, update = false)
+
+## objective lenses
+obj_lens_1 = objective_lens_1()
+obj_lens_2 = objective_lens_2()
+tube_lens = tube_doublet()
+
+## lens 1 render
+fig = Figure(size=(600, 200)) 
+display(fig)
+ax = LScene(fig[1,1])
+hide_axis(ax)
+render!(ax, obj_lens_1, transparency=false, color=lens_color())
+
+c_view = [
+ -0.704021   0.710179  2.22045e-16  -0.000263177
+ -0.412543  -0.408966  0.813975      0.000154901
+  0.578068   0.573056  0.5809       -0.00358713
+  0.0        0.0       0.0           1.0
+]
+
+set_view(ax, c_view)
+save("objective_lens_1.png", fig; px_per_unit=8, update = false)
 
 ##
-lg = objective_group()
+fig = Figure(size=(600, 200)) 
+display(fig)
+ax = LScene(fig[1,1])
+hide_axis(ax)
+render!(ax, obj_lens_2, transparency=true, color=lens_color(0.25))
 
-xrotate3d!(lg, deg2rad(90))
+arrow!(ax, [0,0,0], [1,0,0]; color=:red)
+arrow!(ax, [0,0,0], [0,1,0]; color=:green)
+arrow!(ax, [0,0,0], [0,0,1]; color=:blue)
 
-l1 = lg.objects[1]
-l2 = lg.objects[2]
-l3 = lg.objects[3]
+text!(Point3(5mm, 0, 1mm), text="x")
+text!(Point3(0, 5mm, 1mm), text="y")
+text!(Point3(0, 0mm, 6.5mm), text="z")
 
+c_view = [
+ -0.629489    0.777009  5.55112e-17  -0.000467742
+ -0.0282456  -0.022883  0.999339     -0.00229113
+  0.776496    0.629073  0.0363517    -0.0134646
+  0.0         0.0       0.0           1.0
+]
+
+set_view(ax, c_view)
+save("objective_lens_2.png", fig; px_per_unit=8, update = false)
+
+## full group render
+translate_to3d!(obj_lens_2, [0, BMO.position(obj_lens_1)[2] + BMO.thickness(obj_lens_1) + 3.344mm, 0])
+translate_to3d!(tube_lens, [0, BMO.position(obj_lens_2)[2] + BMO.thickness(obj_lens_2) + 2mm, 0])
 
 fig = Figure(size=(600, 300))
 display(fig)
 ax = LScene(fig[1,1])
 hide_axis(ax)
-render!(ax, l1, color=RGBAf(1, 0, 0, .25))
-render!(ax, l2, color=RGBAf(0, 1, 0, .25))
-render!(ax, l3, color=RGBAf(0, 0, 1, .25))
+render!(ax, obj_lens_1, transparency=true, color=lens_color(0.5))
+render!(ax, obj_lens_2, transparency=true, color=lens_color(0.5))
+render!(ax, tube_lens, transparency=true, color=lens_color(0.5))
+
+tp1 = BMO.position(obj_lens_1) + [0, -.5mm, 2mm]
+tp2 = BMO.position(obj_lens_2) + [0, .2mm, 3mm]
+tp3 = BMO.position(tube_lens) + [0, 1.3mm, 4mm]
+
+text!(tp1, text="obj_lens_1")
+text!(tp2, text="obj_lens_2")
+text!(tp3, text="tube_lens")
+
+lines!(ax, [0,0], [-1,1], [0,0], color=:black, transparency=true, linestyle=:dashdot)
 
 set_orthographic(ax)
 
 c_view = [
-  0.00382847  -0.999993    1.11998e-16  -0.00708868
-  0.118809     0.00045486  0.992917      4.83965e-5
- -0.99291     -0.00380136  0.11881      -0.432527
-  0.0          0.0         0.0           1.0
+ -0.010335   0.999947      1.02999e-17  -0.00703319
+  0.0811125  0.000838342   0.996705     -0.000536583
+  0.996651   0.0103009    -0.0811168    -0.460085
+  0.0        0.0           0.0           1.0
 ]
 
 set_view(ax, c_view)
+save("full_objective_lens.png", fig; px_per_unit=8, update = false)
--- docs/src/tutorials/microscope.md
+++ docs/src/tutorials/microscope.md
@@ -22,7 +22,7 @@ The UCLA Miniscope is a lightweight microscope that utilizes 2-photon fluorescen
 
 All specifications for the optical system and CAD files are taken from the UCLA miniscope [repository](https://github.com/golshanilab/UCLA_2P_Miniscope) under GNU GPL-3.0.
 
-![Test](test.png)
+![UCLA miniscope introduction figure](ucla_intro_fig.png)
 
 ## How to follow this tutorial
 
@@ -33,7 +33,16 @@ path = normpath(tutorial_dir) # hide
 @info "Files located at:" path # hide
 ```
 
-If you want to reproduce the plots in this tutorial by yourself, it is recommend that you use the `render!` function as described in the [Visualization](@ref) section.
+If you want to reproduce the plots in this tutorial by yourself, it is recommend that you use the `render!` function as described in the [Visualization](@ref) section. In addition, your simulation script to follow this tutorial should include the following setup code at the start of the file:
+
+```julia
+using GLMakie, BeamletOptics
+
+const BMO = BeamletOptics
+
+const cm = 1e-2
+const mm = 1e-3
+```
 
 ## Miniscope objective lenses
 
@@ -43,38 +52,63 @@ This tutorial will focus on modeling the return path of the fluorescence light. 
 2. Use the [`SphericalLens`](@ref) convenience constructor
 3. Utilize the quasi surface-based [`Lens`](@ref) constructor
 
-Due to its versatility, we will use the latter method in order to reimplement the Zemax design file. It is based on the definition of surface types which will be translated into the correct `AbstractShape` automatically. Refer to the [Surface based lens construction](@ref) section for more information. The following code defines the front planar surface and the back spherical convex surface.
+Due to its versatility, we will use the latter method in order to reimplement the Zemax design file. It is based on the definition of surface types which will be translated into the correct `AbstractShape` automatically. Refer to the [Surface based lens construction](@ref) section for more information.
 
-**FIXME: explain ref. index**
+### Defining refractive index data
+
+Before we can start, we will need to define refractive indices as a function of the wavelength. For the purpose of this tutorial, we will need to consider the refractive index of the relevant glasses at the design wavelengths. Later on we will define geometrical beams with discrete wavelengths for the ray tracing solver. For the purpose of fluorescence imaging, we will look at $\lambda_{green} = 546.1~\mathrm{nm}$ and $\lambda_{red} = 656.3~\mathrm{nm}$. The indices have been manually taken from the [Schott glass catalog](https://www.schott.com/shop/advanced-optics/en/Optical-Glass/c/optical-glass).
 
 ```julia
-# Define the ref. index
-NBK7 = DiscreteRefractiveIndex([532e-9, 1064e-9], [1.5195, 1.5066])
+# Define the design wavelengths
+λ_green = 546.1e-9
+λ_red   = 656.3e-9
+lambdas = [λ_green, λ_red]
+# Define ref. indices at the specified wavelengths
+NBK7    = DiscreteRefractiveIndex(lambdas, [1.51872, 1.51432])
+NSK5    = DiscreteRefractiveIndex(lambdas, [1.59142, 1.58619])
+NSF4    = DiscreteRefractiveIndex(lambdas, [1.76164, 1.74719])
+NLAK22  = DiscreteRefractiveIndex(lambdas, [1.65391, 1.64760])
+NLASF44 = DiscreteRefractiveIndex(lambdas, [1.80832, 1.79901])
+```
 
+Note that the tupel ($\lambda_{1}$, $\lambda_{2}$) is mapped onto ($n_{1}$, $n_{2}$). For more information, refer to the [`DiscreteRefractiveIndex`](@ref) docs.
+
+### Specifying the lens shape
+
+The following code defines the front planar surface and the back spherical convex surface.
+
+```julia
 # Objective lens 1
 surf_1 = CircularFlatSurface(2*1.144mm)
 surf_2 = SphericalSurface(-1.448mm, 2*1.144mm)
-obj_lens_1 = Lens(surf_1, surf_2, 1.3mm, NBK7)
+obj_lens_1 = Lens(surf_1, surf_2, 1.3mm, NSK5)
 ```
 
 Note that `obj_lens_1` is a single `Lens` entity once spawned and can be manipulated in 3D-space as described in the section: [Moving optical elements](@ref).
+
+![First objective lens](objective_lens_1.png)
 
 ### Building the objective group
 
 To build the second and third lens elements of the objective group we will proceed as above. However, these lenses are spherical doublets formed by two lenses bonded with an optical adhesive. They are also more complex in shape, featuring a mechanical outer diameter. For the second lens the generating code is provided below. Two [`Lens`](@ref)es can be combined into a [`DoubletLens`](@ref). Correct "assembly" of the lens parts is the responsibility of the user.
 
 ```julia
-# Objective lens 2
+# Objective lens 2 - define surfaces
 surf_1 = SphericalSurface(38.184mm, 2*1.840mm, 2*2.380mm)
 surf_2 = SphericalSurface(3.467mm, 2*2.060mm, 2*2.380mm)
 surf_3 = SphericalSurface(-5.020mm, 2*2.380mm)
-dl11 = Lens(surf_1, surf_2, 0.5mm, NBK7)
-dl12 = Lens(surf_2, surf_3, 2.5mm, NBK7)
+# Define front and back lens
+dl11 = Lens(surf_1, surf_2, 0.5mm, NSF4)
+dl12 = Lens(surf_2, surf_3, 2.5mm, NLAK22)
+# Move back lens into position
 translate3d!(dl12, [0, BMO.thickness(dl11), 0])
+# Spawn doublet
 obj_lens_2 = DoubletLens(dl11, dl12)
 ```
 
 The lens parts `dl11` and `dl12` are joined together by moving `dl12` along the y-axis by a distance equal to the on-axis thickness of `dl11`.
+
+![Second objective lens](objective_lens_2.png)
 
 !!! info "Axis conventions"
     In general, most components provided by **BMO** are aligned with the global y-axis and spawned at the origin unless specified otherwise.
@@ -86,8 +120,8 @@ Similarily, the third objective lens can be reproduced from the available Zemax 
 surf_1 = SphericalSurface(7.744mm, 2*2.812mm, 2*3mm)
 surf_2 = SphericalSurface(-3.642mm, 2*3mm)
 surf_3 = SphericalSurface(-14.413mm, 2*2.812mm, 2*3mm)
-dl21 = Lens(surf_1, surf_2, 3.4mm, NBK7)
-dl22 = Lens(surf_2, surf_3, 1.0mm, NBK7)
+dl21 = Lens(surf_1, surf_2, 3.4mm, NLAK22)
+dl22 = Lens(surf_2, surf_3, 1.0mm, NSF4)
 translate3d!(dl22, [0, BMO.thickness(dl21), 0])
 tube_lens = DoubletLens(dl21, dl22)
 ```
@@ -103,6 +137,27 @@ objective_group = ObjectGroup([obj_lens_1, obj_lens_2, tube_lens])
 The `objective_group` locks all elements in place with respect to their relative positions and allows for combined translations and rotations of the elements.
 
 ### Visualizing the lenses
+
+In order to visualize what we have done so far, we will use `GLMakie` to create a 3D representation of the `BMO` geometry representation. The following code will be used.
+
+```julia
+# define rgba for lens look
+lens_color() = RGBf(0.678, 0.847, 0.902)
+lens_color(alpha) = RGBAf(0.678, 0.847, 0.902, alpha)
+
+# generate figure
+fig = Figure(size=(600, 300))
+display(fig)
+ax = LScene(fig[1,1])
+hide_axis(ax)
+render!(ax, obj_lens_1, transparency=true, color=lens_color(0.5))
+render!(ax, obj_lens_2, transparency=true, color=lens_color(0.5))
+render!(ax, tube_lens, transparency=true, color=lens_color(0.5))
+```
+
+The generated figure can be seen below. A dashdot line has been added to indicate the global y-axis and lens labels are shown for easier viewing.
+
+![Miniscope objective lenses](full_objective_lens.png)
 
 ## The rest
 
