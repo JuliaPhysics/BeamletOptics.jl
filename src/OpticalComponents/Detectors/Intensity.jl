b415624ad27708c97b3420ada917d508623c3623
= <hugo.uittenbosch@dlr.de>
2025-09-23 18:18:17 +0200

    fix issue #36

--- src/OpticalComponents/Detectors/Intensity.jl
+++ src/OpticalComponents/Detectors/Intensity.jl
@@ -1,45 +1,45 @@
 electric_field(d::Detector; kwargs...) = electric_field(d, hits(d); kwargs...)
 
+electric_field(d::Detector, ::Nothing; kwargs...) = throw(ErrorException("No hits available on detector."))
+
 function electric_field(
         pd::Detector,
         hits::Vector{GaussianBeamletHit{G}};
         n::Int=100,
         kwargs...
     ) where G
-    # Preallocate transforms
-    T = transpose(orientation(shape(pd)))
-    p = position(shape(pd))
     # Preallocate x-/y-buffers and field
-    xs = ys = LinRange(-pd.edgln/2, pd.edgln/2, n)
+    xs = zs = LinRange(-pd.edgln/2, pd.edgln/2, n)
     field = zeros(Complex{G}, n, n)
+    # PD local coordinate axes (left-handed coords.)
+    local_x = Point3(-orientation(pd)[:,1])  # flipped sign due to rotated pd mesh
+    local_z = Point3(orientation(pd)[:,3])
+    origin = position(pd)
     # Calculate field superposition
-    Threads.@threads for j in eachindex(ys) # FIXME row column major order?
-        y = ys[j]
-        @inbounds for i in eachindex(xs)
+    for j in eachindex(zs) # FIXME row column major order?
+        z = zs[j]
+        for i in eachindex(xs)
             x = xs[i] 
             # Transform point p on PD into world coordinates
-            p1 = Point3(
-                T[1, 1] * x + T[1, 3] * y + p[1],
-                T[2, 1] * x + T[2, 3] * y + p[2],
-                T[3, 1] * x + T[3, 3] * y + p[3]
-            )
+            p1 = origin + x * local_x + z * local_z
             # Add E-field contribution for each hit
             for hit in hits
                 p0 = position(hit)
                 d0 = direction(hit)
+                # important: calculate l0 with geometric length for local r, z
                 l0 = length(hit)
                 proj = projection_factor(hit)
                 # Find projection of p1 onto Gaussian optical axis, i.e. local r and z
                 l1 = dot(p1 - p0, d0)
                 p2 = p0 + l1 * d0
-                r = norm(p1 - p2)
-                z = l0 + l1
+                r_gb = norm(p1 - p2)
+                z_gb = l0 + l1
                 # Add field contribution, projection factor accounts for beam spot stretching
-                field[i, j] += electric_field(hit.gauss, r, z) * sqrt(proj)
+                field[i, j] += electric_field(hit.gauss, r_gb, z_gb) * sqrt(proj)
             end
         end
     end
-    return xs, ys, field
+    return xs, zs, field
 end
 
 function electric_field(
@@ -84,7 +84,7 @@ function electric_field(
             # Add all field contributions
             acc = zero(complex(R))
             @inbounds @simd for hit in hits
-                l = dot(p - position(hit), direction(hit))
+                l = dot(p - hit_point(hit), direction(hit))
                 acc += projection_factor(hit) * cis(wavenumber(hit) * (optical_path_length(hit) + l))
             end
             field[i,j] = acc
