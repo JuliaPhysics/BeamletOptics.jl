6a31d698f5611e2229340c134fe358e88692f04b
= <hugo.uittenbosch@dlr.de>
2025-10-24 15:44:46 +0200

    add stop/cont. option to IntersectableObject

--- src/OpticalComponents/Intersectable.jl
+++ src/OpticalComponents/Intersectable.jl
@@ -6,12 +6,40 @@ A passive [`AbstractObject`](@ref) which can be hit by a beam. In this case, the
 # Fields
 
 - `shape`: an [`AbstractShape`](@ref)
+- `stop`: Boolean flag indicating if rays can pass through the object, default is `true`
 """
-struct IntersectableObject{T, S <: AbstractShape{T}} <: AbstractObject{T, S}
-    shape::S
+mutable struct IntersectableObject{T, S <: AbstractShape{T}} <: AbstractObject{T, S}
+    const shape::S
+    stop::Bool
 end
 
-set_new_origin3d!(d::IntersectableObject) = set_new_origin3d!(d.shape)
-interact3d(::AbstractSystem, ::IntersectableObject, ::AbstractBeam, ::AbstractRay) = nothing
+stop(io::IntersectableObject) = io.stop
 
-IntersectableObject(loadpath::String) = IntersectableObject(Mesh(load(loadpath)))
+set_new_origin3d!(d::IntersectableObject) = set_new_origin3d!(d.shape)
+
+function interact3d(::AbstractSystem, ::IntersectableObject, ::AbstractBeam, ::AbstractRay)
+    @warn "Continue tracing not implemented for this beam type..."
+    return nothing
+end
+
+function interact3d(::AbstractSystem, io::IntersectableObject, beam::Beam{T, R}, ray::R) where {T <: Real, R <: Ray{T}}
+    if stop(io)
+        return nothing
+    end
+
+    npos = position(ray) + length(ray) * direction(ray)
+    ndir = direction(ray)
+
+    return BeamInteraction{T, R}(
+        nothing,
+        Ray{T}(
+            npos,
+            ndir, 
+            nothing,
+            wavelength(ray),
+            refractive_index(ray)
+        )
+    )
+end
+
+IntersectableObject(loadpath::String, stop=true) = IntersectableObject(Mesh(load(loadpath)), stop)
