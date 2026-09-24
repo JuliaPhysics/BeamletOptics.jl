6750ba80f79c294a2d7b7dcc6aa1ca5c4090ee3a
Hugo Uittenbosch <hugo.uittenbosch@dlr.de>
2026-09-24 16:08:40 +0200

    unify config getter

--- src/SDFs/AbstractSDF.jl
+++ src/SDFs/AbstractSDF.jl
@@ -184,12 +184,12 @@ function intersect3d(object::AbstractSDF, ray::AbstractRay)
     pos = position(ray)
     dir = direction(ray)
     d = sdf(object, pos)
+    h = Config.get_sdf_surface_threshold()
     # Test if outside of sdf, else inside
-    if d > Config.get_sdf_surface_threshold()
+    if d > h
         return _raymarch_outside(object, pos, dir)
     end
     # The one-sided difference along the ray is used to determine inside/outside instead of the normal
-    h = Config.get_sdf_surface_threshold()
     if sdf(object, pos + h * dir) ≤ d
         return _raymarch_inside(object, pos, dir)
     else
